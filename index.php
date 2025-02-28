2c82d4241a067ad78cf077b28ecf4ccc1e567fa6
judinus10 <judes.20222403@iit.ac.lk>
2025-02-28 21:16:05 +0530

    code for the index.php file added

--- index.php
+++ index.php
@@ -1,26 +1,32 @@
-<?php 
-$pagename="template";      
-//Create and populate a variable called $pagename 
-echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";  
-echo "<title>".$pagename."</title>";   
-echo "<body>"; 
-include ("headfile.html");     
-echo "<h4>".$pagename."</h4>";    
-//Call in stylesheet 
-//display name of the page as window title 
-//include header layout file  
-//display name of the page on the web page 
-//display random text 
-echo "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-labore et dolore magna aliqua. Non consectetur a erat nam at lectus urna. Cras pulvinar mattis nunc sed 
-blandit libero volutpat sed cras. Nunc aliquet bibendum enim facilisis gravida neque convallis a cras. 
-Nunc consequat interdum varius sit. Nam aliquam sem et tortor consequat. Magna sit amet purus gravida. Non 
-sodales neque sodales ut etiam sit. Tortor consequat id porta nibh venenatis. Ornare arcu odio ut sem 
-nulla pharetra diam. Tincidunt ornare massa eget egestas purus. Pulvinar mattis nunc sed blandit libero 
-volutpat sed. Nulla malesuada pellentesque elit eget. Varius quam quisque id diam vel quam elementum 
-pulvinar. Aliquet eget sit amet tellus cras adipiscing enim eu turpis. Vestibulum lectus mauris ultrices 
-eros in. Faucibus in ornare quam viverra. Hac habitasse platea dictumst vestibulum rhoncus. Parturient 
-montes nascetur ridiculus mus. Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna."; 
-include("footfile.html");     
-echo "</body>"; 
-?> 
+<?php
+include ("db.php"); //include db.php file to connect to DB
+$pagename="Make your home smart"; //create and populate variable called $pagename
+echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";
+echo "<title>".$pagename."</title>";
+echo "<body>";
+include ("headfile.html");
+echo "<h4>".$pagename."</h4>";
+//create a $SQL variable and populate it with a SQL statement that retrieves product details
+$SQL="select prodId, prodName, prodPicNameSmall from Product";
+//run SQL query for connected DB or exit and display error message
+$exeSQL=mysqli_query($conn, $SQL) or die (mysqli_error($conn));
+echo "<table style='border: 0px'>";
+//create an array of records (2 dimensional variable) called $arrayp.
+//populate it with the records retrieved by the SQL query previously executed.
+//Iterate through the array i.e while the end of the array has not been reached, run through it
+while ($arrayp=mysqli_fetch_array($exeSQL))
+{
+echo "<tr>";
+echo "<td style='border: 0px'>";
+//display the small image whose name is contained in the array
+echo "<img src=images/".$arrayp['prodPicNameSmall']." height=200 width=200>";
+echo "</td>";
+echo "<td style='border: 0px'>";
+echo "<p><h5>".$arrayp['prodName']."</h5>"; //display product name as contained in the array
+echo "</td>";
+echo "</tr>";
+}
+echo "</table>";
+include ("footfile.html");
+echo "</body>";
+?>

4865dbe4d4476d774127c2ec12333599e84406ae
Judinus10 <judes.20222403@iit.ac.lk>
2025-07-10 16:13:24 +0530

    modified

--- basket.php
+++ basket.php
@@ -1,5 +1,5 @@
 <?php
-session_start(); // Start the session
+session_start();
 include("db.php");
 
 $pagename = "Smart Basket";
@@ -9,87 +9,66 @@ echo "<body>";
 include("headfile.html");
 echo "<h4>" . $pagename . "</h4>";
 
-//if the posted ID of the new product is set i.e. if the user is adding a new product into the basket
-if (isset($_POST['h_prodid'])) {
-    //capture the ID of selected product using the POST method and the $_POST superglobal variable
-//and store it in a new local variable called $newprodid
-    $newprodid = $_POST['h_prodid'];
-    //capture the required quantity of selected product using the POST method and $_POST superglobal variable
-//and store it in a new local variable called $reququantity
-    $reququantity = $_POST['p_quantity'];
-    //Display id of selected product
-    // echo "<p>Id of selected product: " . $newprodid;
-    //Display quantity of selected product
-    // echo "<br>Quantity of selected product: " . $reququantity;
-    //create a new cell in the basket session array. Index this cell with the new product id.
-//Inside the cell store the required product quantity
-    $_SESSION['basket'][$newprodid] = $reququantity;
-    //Display "1 item added to the basket " message
-    echo "<p>1 item added";
-}
-//else
-//Display "Current basket unchanged " message
-else {
-    // echo "<p>Basket unchanged";
+/* ✅ REMOVE ITEM CODE (as per your pseudocode) */
+if (isset($_POST['del_prodid'])) {
+    $delprodid = $_POST['del_prodid'];
+    unset($_SESSION['basket'][$delprodid]);
+    echo "<p>1 item removed</p>";
 }
 
-// Initialize total to zero
-$total = 0; //Create a variable $total and initialize it to zero
-//Create HTML table with header to display the content of the basket: prod name, price, selected quantity and subtotal
+/* ✅ ADD ITEM TO BASKET */
+if (isset($_POST['h_prodid']) && isset($_POST['p_quantity'])) {
+    $newprodid = $_POST['h_prodid'];
+    $reququantity = $_POST['p_quantity'];
+    $_SESSION['basket'][$newprodid] = $reququantity;
+    echo "<p>1 item added</p>";
+}
+
+$total = 0;
 echo "<p><table id='baskettable'>";
 echo "<tr>";
 echo "<th>Product Name</th><th>Price</th><th>Quantity</th><th>Subtotal</th><th>Remove Product</th>";
 echo "</tr>";
-//if the session array $_SESSION['basket'] is set
-if (isset($_SESSION['basket'])) {
-    //loop through the basket session array for each data item inside the session using a foreach loop
-//to split the session array between the index and the content of the cell
-//for each iteration of the loop
-//store the id in a local variable $index & store the required quantity into a local variable $value
+
+if (isset($_SESSION['basket']) && !empty($_SESSION['basket'])) {
     foreach ($_SESSION['basket'] as $index => $value) {
-        //SQL query to retrieve from Product table details of selected product for which id matches $index
-//execute query and create array of records $arrayp
         if (!is_numeric($index)) {
-            continue; // Skip if index is not a number
+            continue;
         }
-        $SQL = "select prodId, prodName, prodPrice from Product where prodId=" . $index;
+
+        $SQL = "SELECT prodId, prodName, prodPrice FROM Product WHERE prodId=" . intval($index);
         $exeSQL = mysqli_query($conn, $SQL) or die(mysqli_error($conn));
         $arrayp = mysqli_fetch_array($exeSQL);
+
+        $subtotal = $arrayp['prodPrice'] * $value;
+        $total += $subtotal;
+
         echo "<tr>";
-        //display product name & product price using array of records $arrayp
         echo "<td>" . $arrayp['prodName'] . "</td>";
         echo "<td>&pound" . number_format($arrayp['prodPrice'], 2) . "</td>";
-        // display selected quantity of product retrieved from the cell of session array and now in $value
         echo "<td style='text-align:center;'>" . $value . "</td>";
-        //calculate subtotal, store it in a local variable $subtotal and display it
-        $subtotal = $arrayp['prodPrice'] * $value;
         echo "<td>&pound" . number_format($subtotal, 2) . "</td>";
-        echo "<form action=basket.php method=post>";
-        echo "<td>";
-        echo "<input type=submit value='Remove' id='submitbtn'>";
-        echo "</td>";
-        echo "<input type='hidden' name='remove_prodid' value='" . $arrayp['prodId'] . "'>";
-        echo "</form>";
+
+        // ✅ Proper form inside one <td>
+        echo "<td>
+                <form action='basket.php' method='post'>
+                    <input type='hidden' name='del_prodid' value='" . $arrayp['prodId'] . "'>
+                    <input type='submit' value='Remove' id='submitbtn'>
+                </form>
+              </td>";
         echo "</tr>";
-        //increase total by adding the subtotal to the current total
-        $total = $total + $subtotal;
     }
+
+    echo "<tr><td colspan='4'><b>TOTAL</b></td><td><b>&pound" . number_format($total, 2) . "</b></td></tr>";
+} else {
+    echo "<tr><td colspan='5'>Your basket is empty</td></tr>";
 }
-//else display empty basket message
-else {
-    echo "<p>Empty basket";
-}
-// Display total
-echo "<tr>";
-echo "<td colspan=4>TOTAL</td>";
-echo "<td>&pound" . number_format($total, 2) . "</td>";
-echo "</tr>";
+
 echo "</table>";
 
 echo "<br><p><a href='clearbasket.php'>CLEAR BASKET</a></p>";
-
 echo "<br><p>New homteq customers: <a href='signup.php'>Sign up</a></p>";
-echo "<p>Returning homteq customers: <a href='login.php'>Login</a></p>";
+echo "<br><p>Returning homteq customers: <a href='login.php'>Login</a></p>";
 
 include("footfile.html");
 echo "</body>";
